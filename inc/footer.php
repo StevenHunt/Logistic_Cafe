67085a0a1288ea64e446b8d742eb6f75cd073dee
Steven <Steven.c.hunt@gmail.com>
2018-07-31 23:41:44 -0700

    image != Image

--- inc/footer.php
+++ inc/footer.php
@@ -67,10 +67,10 @@
             </div>
         
             <div class="social"> 
-                <a href="https://www.twitter.com" target="_blank"><img src="../images/Footer/twitter.png"> </a> &nbsp; &nbsp; 
-                <a href="https://www.facebook.com" target="_blank"><img src="../images/Footer/facebook.png"> </a> &nbsp; &nbsp; 
-                <a href="https://www.linkedin.com" target="_blank"><img src="../images/Footer/Linkedin.png"> </a> &nbsp; &nbsp; 
-                <a href="https://www.instagram.com" target="_blank"><img src="../images/Footer/instagram.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.twitter.com" target="_blank"><img src="../Images/Footer/twitter.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.facebook.com" target="_blank"><img src="../Images/Footer/facebook.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.linkedin.com" target="_blank"><img src="../Images/Footer/Linkedin.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.instagram.com" target="_blank"><img src="../Images/Footer/instagram.png"> </a> &nbsp; &nbsp; 
             </div>
     </div>
 
