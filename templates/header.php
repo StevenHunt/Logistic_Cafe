bfe1da48db54d575b2a97763c8b36bafc48349d7
Steven <Steven.c.hunt@gmail.com>
2018-07-31 23:16:09 -0700

    CSS updates and folder change
    Bootstrapping and changing the location to a more standardized folder.

--- templates/header.php
+++ templates/header.php
@@ -1,33 +1,64 @@
+<!DOCTYPE html>
 <html>
 <head>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="keywords" content="HTML5 Template" />
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Logistic Cafe &amp; Grill</title>
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="images/favicon.png">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="js/isotope/isotope.css">
+	<link rel="stylesheet" href="js/flexslider/flexslider.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.theme.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.transitions.css">
+	<link rel="stylesheet" href="js/superfish/css/megafish.css" media="screen">
+	<link rel="stylesheet" href="js/superfish/css/superfish.css" media="screen">
+	<link rel="stylesheet" href="js/pikaday/pikaday.css">
+	<link rel="stylesheet" href="css/settings-panel.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/light.css">
+    <link rel="stylesheet" hret="css/footer.css">
+	<link rel="stylesheet" href="css/responsive.css">
+
+	<!-- JS Font Script -->
+	<script src="http://use.edgefonts.net/bebas-neue.js"></script>
     
-<title>Logistic Cafe &amp; Grill</title>
+    <!-- Modernizer -->
+	<script src="js/modernizr.custom.js"></script>
 
-    <!-- Icon --> 
-    <link rel="shortcut icon" type="image/x-icon" href="../Images/favicon.ico" />    
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    <!-- CSS --> 
-    <link rel="stylesheet" href="../CSS/style.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
+    <!-- Alert Box --> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 </head>
- 
-<body>
+<body class="multi-page">
+
+<div id="wrapper" class="wrapper">
     
-    <div class="container"> 
-       <div class="row">
-          <div class="col-md-5">
-              <!-- Home --> 
-              <a class="img-responsive logo" href="http://ec2-54-213-179-65.us-west-2.compute.amazonaws.com"><img src="../Images/LGC_Logo.png" /></a>
-          </div>
-       </div>
-    </div>
+	<!-- HEADER -->
+	<header id="header2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-xs-5 logo">
+					<a href="index.php"><img src="http://[::1]/cishop/assets/images/logo.png" class="img-responsive" alt=""/></a>
+				</div>
+				<div class="col-md-9 col-xs-7">
+					<div class="top-bar">
+						
+					</div>
+				</div>
+			</div>
