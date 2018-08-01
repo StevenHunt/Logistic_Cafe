81f674dedae8c9875228b44d60e6a6df4b16bd0a
Steven <Steven.c.hunt@gmail.com>
2018-07-31 23:13:10 -0700

    Dynamic changes
    Added in-file styling as well.

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,79 @@
-        <div id="footer">
+<html>
+
+<head> 
+    
+    <style> 
+        
+	    #footer {
+	        position: fixed; 
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #000000;
+            color: white;
+        }
+
+        a.social {
+            padding: 0px 5px; 
+        }
+
+        .social {
+            float: right;
+            padding-right: 20px;
+            padding-top: 5px;
+        }
+
+        .copyright {
+            float: left;
+            color: white;
+            padding-left: 20px; 
+        }
+        
+    </style> 
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/dialogFx.js"></script>
+    <script src="js/dialog-js.js"></script>
+    <script src="js/navigation/jquery.easing.js"></script>
+    <script src="js/flexslider/jquery.flexslider.js"></script>
+    <script src="js/navigation/scroll.js"></script>
+    <script src="js/navigation/jquery.sticky.js"></script>
+    <script src="js/owl-carousel/owl.carousel.min.js"></script>
+    <script src="js/isotope/isotope.pkgd.js"></script>
+    <script src="js/superfish/js/hoverIntent.js"></script>
+    <script src="js/superfish/js/superfish.js"></script>
+    <script src="js/tweecool.js"></script>
+    <script src="js/jquery.bpopup.js"></script>
+    <script src="js/pikaday/pikaday.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+    <script src="js/rs-plugin/js/jquery.themepunch.tools.min.js"></script>   
+    <script src="js/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="js/jquery.prettyphoto.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/booking.js"></script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script src="js/gmap.js"></script>
+    <script src="js/gmap2.js"></script>
+
+</head>
+<body> 
+    <div id="footer">
+        
             <div class="copyright"> 
                 <h5 style="padding-left: 10px"> &copy; 2018 Logistic Cafe &amp; Grill. All Rights Reserved. </h5>
             </div>
+        
             <div class="social"> 
-                <a href="https://www.twitter.com" target="_blank"><img src="Images/Footer/twitter.png"> </a> &nbsp; &nbsp; 
-                <a href="https://www.facebook.com" target="_blank"><img src="Images/Footer/facebook.png"> </a> &nbsp; &nbsp; 
-                <a href="https://www.linkedin.com" target="_blank"><img src="Images/Footer/Linkedin.png"> </a> &nbsp; &nbsp; 
-                <a href="https://www.instagram.com" target="_blank"><img src="Images/Footer/instagram.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.twitter.com" target="_blank"><img src="../images/Footer/twitter.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.facebook.com" target="_blank"><img src="../images/Footer/facebook.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.linkedin.com" target="_blank"><img src="../images/Footer/Linkedin.png"> </a> &nbsp; &nbsp; 
+                <a href="https://www.instagram.com" target="_blank"><img src="../images/Footer/instagram.png"> </a> &nbsp; &nbsp; 
             </div>
-        </div>
-    </body>
+    </div>
+
+
+</body>
 </html>
