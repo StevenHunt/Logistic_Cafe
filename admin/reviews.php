76a0d61c11103b65f67605cd2aa8be627f3a5f17
Steven <Steven.c.hunt@gmail.com>
2018-08-07 20:40:56 -0700

    Reformatting

--- admin/reviews.php
+++ admin/reviews.php
@@ -1,12 +1,17 @@
 <?php
+
 	session_start();
 	require_once '../config/connect.php';
-	if(!isset($_SESSION['email']) & empty($_SESSION['email'])){
+
+    if(!isset($_SESSION['email']) & empty($_SESSION['email'])) {
 		header('location: login.php');
 	}
+	
+	$page_title = "Reviews";
+	include 'inc/header.php';
+	include 'inc/nav.php'; 
+
 ?>
-<?php include 'inc/header.php'; ?>
-<?php include 'inc/nav.php'; ?>
 	
 <section id="content">
 	<div class="content-blog">
@@ -23,8 +28,12 @@
 				</thead>
 				<tbody>
 				<?php 	
-					$sql = "SELECT r.id, p.name, r.review, r.`timestamp` FROM reviews r JOIN products p WHERE r.pid=p.id";
-					$res = mysqli_query($connection, $sql); 
+					$sql = "SELECT r.id, p.name, r.review, r.`timestamp` 
+                            FROM reviews r 
+                            JOIN products p 
+                            WHERE r.pid=p.id";
+					
+                    $res = mysqli_query($connection, $sql); 
 					while ($r = mysqli_fetch_assoc($res)) {
 				?>
 					<tr>
@@ -40,6 +49,6 @@
 			
 		</div>
 	</div>
-
 </section>
-<?php include 'inc/footer.php' ?>
+</body> 
+</html> 
