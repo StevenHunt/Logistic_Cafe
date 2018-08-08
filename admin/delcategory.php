3ca8f75b43fe25c55094b605582d8aeca18e8988
Steven <Steven.c.hunt@gmail.com>
2018-08-07 20:33:07 -0700

    Functional Approach

--- admin/delcategory.php
+++ admin/delcategory.php
@@ -1,26 +1,23 @@
 <?php
+
 	session_start();
 	require_once '../config/connect.php';
 
-	if(!isset($_SESSION['email']) & empty($_SESSION['email'])) {
-        
+	if(!isset($_SESSION['email']) & empty($_SESSION['email'])) {        
 		header('location: login.php');
 	}
 
-	if(isset($_GET) & !empty($_GET)) {
-        
+	if(isset($_GET) & !empty($_GET)) {     
 		$id = $_GET['id'];
         
 		$sql = "DELETE FROM category WHERE id='$id'";
         
-		if(mysqli_query($connection, $sql)) {     
-            
+		if(mysqli_query($connection, $sql)) {                 
 			header('location:categories.php');
 		}
 	}
 
-    else {
-        
+    else {   
 		header('location: categories.php');
 	}
 ?>
